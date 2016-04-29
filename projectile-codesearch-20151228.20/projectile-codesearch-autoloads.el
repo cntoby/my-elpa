0699113988c5beb3f8e342033ee9c82711361840
Toby <cntoby@gmail.com>
2016-04-29 11:55:50 +0800

    remove old packages

--- projectile-codesearch-20151228.20/projectile-codesearch-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; projectile-codesearch-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("projectile-codesearch.el") (22306 11020
-;;;;;;  773778 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; projectile-codesearch-autoloads.el ends here
