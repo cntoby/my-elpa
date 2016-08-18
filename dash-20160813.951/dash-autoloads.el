995c445b727cd4998e201dc8b9f03b6bd1d71649
Toby <cntoby@gmail.com>
2016-08-18 20:49:49 +0800

    update

---
+++ dash-20160813.951/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (22450 54149 144793 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
