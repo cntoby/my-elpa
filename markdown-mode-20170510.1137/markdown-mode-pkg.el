092e240225d91fa2d9f900e10046845f3b37ab7d
Toby <cntoby@gmail.com>
2017-05-19 09:20:50 +0800

    update

--- markdown-mode-20170510.1137/markdown-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "markdown-mode" "20170510.1137" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :commit "c9cb29b2e0caa516bf0fc33e0ec6682d4c837445" :url "http://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
