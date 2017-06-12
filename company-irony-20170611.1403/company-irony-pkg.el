13f03127cc33f3cab540c6b1f0928080b93b4200
Toby <cntoby@gmail.com>
2017-06-12 10:57:49 +0800

    update

---
+++ company-irony-20170611.1403/company-irony-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company-irony" "20170611.1403" "company-mode completion back-end for irony-mode" '((emacs "24.1") (company "0.8.0") (irony "1.0.0") (cl-lib "0.5")) :commit "f68c1b46d64b9f95cfc2a6d611089d0442ed58ba" :url "https://github.com/Sarcasm/company-irony/" :keywords '("convenience"))
