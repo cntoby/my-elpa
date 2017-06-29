63d814bfefcbafa8b1827256c49fca99d88f41ec
Toby <cntoby@gmail.com>
2017-06-29 12:46:37 +0800

    update

--- markdown-mode-20170628.1857/markdown-mode-autoloads.el
+++ markdown-mode-20170628.1857/markdown-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22867 34179
+;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22868 34337
 ;;;;;;  0 0))
 ;;; Generated autoloads from markdown-mode.el
 
