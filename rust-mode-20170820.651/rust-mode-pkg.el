c5f71aa884fd121954e92e386fc0606d013f9def
Toby <cntoby@gmail.com>
2017-08-24 18:38:26 +0800

    update

--- rust-mode-20170820.651/rust-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rust-mode" "20170820.651" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :commit "f57a8ebfc33de72e0ca6fe61ed3d7a622510b5b3" :url "https://github.com/rust-lang/rust-mode" :keywords '("languages"))
