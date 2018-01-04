648da0111c0a979bfeb138fb47b5de19d317f8ed
Toby <cntoby@gmail.com>
2018-01-04 21:41:18 +0800

    update

---
+++ codesearch-20171122.431/codesearch-pkg.el
@@ -0,0 +1,7 @@
+(define-package "codesearch" "20171122.431" "Core support for managing codesearch tools"
+  '((elog "0.1"))
+  :url "https://github.com/abingham/emacs-codesearch" :keywords
+  '("tools" "development" "search"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- company-php-20171111.757/company-php-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "company-php" "20171111.757" "company completion source for php" '((cl-lib "0.5") (ac-php-core "1") (company "0.9")) :commit "89b554418de9c95ff430f432f6c780d96d56b634" :url "https://github.com/xcwen/ac-php" :keywords '("completion" "convenience" "intellisense"))
