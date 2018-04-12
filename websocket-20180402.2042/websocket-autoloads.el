866c9e9aaa54dd92209c7653f1c3b69584823de0
Toby <cntoby@gmail.com>
2018-04-12 10:15:47 +0800

    update elpa

---
+++ websocket-20180402.2042/websocket-autoloads.el
@@ -0,0 +1,15 @@
+;;; websocket-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("websocket.el") (23240 29980 351287 457000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; websocket-autoloads.el ends here
