00eb6c2ffed47beac1f1eb8c65ac64a849c058da
Toby <cntoby@gmail.com>
2018-05-21 20:59:04 +0800

    update

--- projectile-codesearch-20171129.600/projectile-codesearch-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "projectile-codesearch" "20171129.600" "Integration of codesearch into projectile" '((codesearch "20171122.431") (projectile "20150405.126")) :commit "4e6a2382c26d1218a4a007d31de2bab8ef9d2e7c" :url "https://github.com/abingham/emacs-codesearch" :keywords '("tools" "development" "search"))
