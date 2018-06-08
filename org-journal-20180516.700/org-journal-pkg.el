12a75b7f6aefc7d6ddfd981bd7466dce1e064cd9
Toby <cntoby@gmail.com>
2018-06-08 09:58:41 +0800

    update

--- org-journal-20180516.700/org-journal-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-journal" "20180516.700" "a simple org-mode based journaling mode" '((emacs "25.1")) :commit "4f7ae1f0c743a383722ebc3428571eedb0b510f5" :url "http://github.com/bastibe/org-journal")
